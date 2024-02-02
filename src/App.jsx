de79d10904cfcbc50a1cff936dd300250005421e
Aaron Latham <latham91@icloud.com>
2024-02-02 23:16:17 +0000

    test

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,26 @@ import Catalog from "./pages/Catalog";
 import Guidelines from "./pages/Guidelines";
 import Checkout from "./pages/Checkout";
 import { SubscriptionProvider } from "./pages/SubscriptionContext";
-import MyAudioComponent from "./Components/Audio.jsx";
+import MyAudioComponent from "./Components/audio.jsx";
 import "./styles/App.css";
 
 const App = () => {
-  return (
-    <Router>
-      <SubscriptionProvider>
-        <div>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/Catalog" element={<Catalog />} />
-            <Route path="/Guidelines" element={<Guidelines />} />
-            <Route path="/Checkout" element={<Checkout />} />
-          </Routes>
-          <MyAudioComponent />
-        </div>
-      </SubscriptionProvider>
-    </Router>
-  );
+    return (
+        <Router>
+            <SubscriptionProvider>
+                <div>
+                    <Navbar />
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/Catalog" element={<Catalog />} />
+                        <Route path="/Guidelines" element={<Guidelines />} />
+                        <Route path="/Checkout" element={<Checkout />} />
+                    </Routes>
+                    <MyAudioComponent />
+                </div>
+            </SubscriptionProvider>
+        </Router>
+    );
 };
 
 export default App;
